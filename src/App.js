0b1d5cb9ce4e4c6c13bf90e6fc4b90e855774bb5
MYSHOGUN <www.789946@hotmail.com>
2023-08-18 15:40:16 +0700

    6th

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import {EventData2} from './event-data'
+import Banner from './banner'
+import { Header , Content , Footer } from './func-components';
+import Calendar from './class-components';
+import Button from './class-components2';
+import Calculator from './calculator'
+import {EventData2,Table} from './event-data'
 
 function App() {
     return ( 
-        <EventData2/>
+        [<Header/>,<Banner/>,<EventData2/>,<Calculator/>,<br/>,<Button/>,<Table/>,<p><Calendar/></p>,<Content/>,<Footer/>]
     );
 }
 
--- src/event-data.js
+++ src/event-data.js
@@ -11,11 +11,34 @@ export function EventData2(){
         let r = eval(t)
         alert(`${t} = ${r}`)
     }
-        return (
-            <div style = {{textAlign:'center',marginTop:20}}>
-                <button onClick={(ev) => onClickStartStop(ev)}>Start</button><br/><br/>
-                <button onClick={(ev) => onClickAdd(ev)}>10 + 20</button><br/><br/>
-                <button onClick={(ev) => onClickAdd(ev)}>30 * 40</button><br/><br/>
-            </div>
-        )
+    return (
+        <div style = {{textAlign:'center',marginTop:20}}>
+            <button onClick={(ev) => onClickStartStop(ev)}>Start</button><br/><br/>
+            <button onClick={(ev) => onClickAdd(ev)}>10 + 20</button><br/><br/>
+            <button onClick={(ev) => onClickAdd(ev)}>30 * 40</button><br/><br/>
+        </div>
+    )
+}
+export function Table(){
+    const data = [['JavaScript',100],['React',150],['React Native',180],['Node.js',200],['VS Code',120]]
+    const onMouseOverRow =(ev)=>{
+        ev.target.parentNode.style.backgroundColor = 'yellow'
+    }
+    const onMouseOutRow = (ev) =>{
+        ev.target.parentNode.style.backgroundColor = 'white'
+    }
+    return (
+        <table border = "1" cellPadding={5} style={{margin:'auto',marginTop:30}}>
+            <tr><th>Product</th><th>Price</th></tr>
+            {
+                data.map(item => {
+                    return (
+                        <tr onMouseOver={onMouseOverRow} onMouseOut={onMouseOutRow}>
+                            <td>{item[0]}</td><td>{item[1]}</td>
+                        </tr>
+                    )
+                })
+            }
+        </table>
+    )
 }
