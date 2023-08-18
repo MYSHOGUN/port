c6db2388e2e194da1795f41bba4eaba2a9a2e571
MYSHOGUN <www.789946@hotmail.com>
2023-08-18 13:50:00 +0700

    3rd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Banner from './banner'
 import { Header , Content , Footer } from './func-components';
 import Calendar from './class-components';
 
 function App() {
     return ( 
-        [<Header/>,<p><Calendar/></p>,<Content/>,<Footer/>]
+        [<Header/>,<Banner/>,<p><Calendar/></p>,<Content/>,<Footer/>]
     );
 }
 
