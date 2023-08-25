5b9638a373906d9f70898d6dff34d78fc6528d99
MYSHOGUN <www.789946@hotmail.com>
2023-08-25 15:07:37 +0700

    7th

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Calendar from './class-components';
 import Button from './class-components2';
 import Calculator from './calculator'
 import {EventData2,Table} from './event-data'
+import RefsFunc from './refs-func';
+import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 function App() {
     return ( 
-        [<Header/>,<Banner/>,<EventData2/>,<Calculator/>,<br/>,<Button/>,<Table/>,<p><Calendar/></p>,<Content/>,<Footer/>]
+        [<Header/>,<Banner/>,<EventData2/>,<MessageBox/>,<Calculator/>,<br/>,<Button/>,<Table/>,<RefsFunc/>,<RefsArray/>,<p><Calendar/></p>,<Content/>,<Footer/>]
     );
 }
 
